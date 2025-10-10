e22ee02a71fddb490305f3d980853a507ab1577e
Sulasulait <148674932+Sulasulait@users.noreply.github.com>
2025-10-10 18:02:09 +0300

    Updated vite.config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/coshikowa">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/get-hired" element={<GetHired />} />
